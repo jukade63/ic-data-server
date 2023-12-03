a9dbba91c3805df95dc132a91c0d85e5e2b75748
jukade63 <jukade63@gmail.com>
2023-12-04 00:50:04 +0700

    s3 image upload complete

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-const aws = require('aws-sdk');
-const dotenv = require('dotenv')
+const express = require("express");
+const mysql = require("mysql2");
+const multer = require('multer')
+const crypto = require('crypto')
+const sharp = require('sharp')
+const cors = require("cors");
+const { uploadFile, deleteFile, getObjectSignedUrl } = require("./s3.js");
 
-dotenv.config()
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -12,77 +16,105 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'ap-southeast-1',
-  signatureVersion: 'v4'
-});
-// const storage = multer.memoryStorage()
-// const upload = multer({ storage: storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const generateFileName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
 
 // MySQL connection configuration
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'cu_test'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "cu_test",
 });
 
 // Connect to MySQL
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL: ' + err.stack);
+    console.error("Error connecting to MySQL: " + err.stack);
     return;
   }
-  console.log('Connected to MySQL');
-
+  console.log("Connected to MySQL");
 });
 
-app.get('/generate-signed-url', (req, res) => {
-  const fileName = req.query.fileName;
-  const fileType = req.query.fileType;
+// create new user
+app.post("/addUser", upload.single("image"), async (req, res) => {
+  console.log(req.body.id);
+  const file = req.file;
+  const id= req.body.id;
+  const firstname = req.body.firstname
+  const lastname = req.body.lastname
+  const DOB = req.body.DOB
+  const address = req.body.address
+  const imageName = generateFileName();
 
-  const s3Params = {
-    Bucket: 'test-icdata',
-    Key: fileName,
-    Expires: 60,
-    ContentType: fileType,
-  };
+  if(file){
+    const fileBuffer = await sharp(file.buffer)
+    .resize({ height: 1920, width: 1080, fit: "contain" })
+    .toBuffer();
+    await uploadFile(fileBuffer, imageName, file.mimetype);
+  }
+ 
+  if (!id || !firstname || !lastname || !DOB || !address) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-  s3.getSignedUrl('putObject', s3Params, (error, signedUrl) => {
-    if (error) {
-      console.error('Error generating signed URL:', error);
-      return res.status(500).json({ error: 'Failed to generate signed URL' });
-    }
-
-    res.json({ signedUrl });
-  });
-});
-
-app.post('/addUser', (req, res) => {
-  console.log('req body',req.body);
-  const { id, firstname, lastname, DOB, address} = req.body;
-  
-    if (!id || !firstname || !lastname || !DOB || !address) {
-        return res.status(400).json({ message: 'All fields are required' });
+  // Insert data into the 'users' table
+  const insertUserQuery = `INSERT INTO users (id, firstname, lastname, DOB, address, imageName) VALUES (?, ?, ?, ?, ?, ?)`;
+  db.query(
+    insertUserQuery,
+    [id, firstname, lastname, DOB, address, imageName],
+    (err, result) => {
+      if (err) {
+        console.error("Error inserting user: " + err.stack);
+        return res.status(500).json({ message: "Error inserting user" });
       }
-    
-      // Insert data into the 'users' table
-      const insertUserQuery = `INSERT INTO users (id, firstname, lastname, DOB, address) VALUES (?, ?, ?, ?, ?)`;
-      db.query(insertUserQuery, [id, firstname, lastname, DOB, address], (err, result) => {
-        if (err) {
-          console.error('Error inserting user: ' + err.stack);
-          return res.status(500).json({ message: 'Error inserting user' });
-        }
-        console.log('User added to the database');
-        res.status(201).json({ message: 'User added successfully' });
-      });
-  });
-  
+      console.log("User added to the database");
+      res.status(201).json({ message: "User added successfully" });
+    }
+  );
+});
 
-// Define your routes
-// ... (same as previous code example)
+// update user data
+app.put("/update-user/:userId", (req, res) => {
+  const updatedUserData = req.body;
+  const userId = updatedUserData.id;
+
+  db.query(
+    "UPDATE users SET ? WHERE id = ?",
+    [updatedUserData, userId],
+    (error, results) => {
+      if (error) {
+        console.error("Error updating user:", error);
+        res.status(500).json({ message: "Error updating user" });
+      } else if (results.affectedRows === 0) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        res.status(200).json({ message: "User data updated successfully" });
+      }
+    }
+  );
+});
+
+// Get user by ID
+app.get("/get-user/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  db.query("SELECT * FROM users WHERE id = ?", [userId], async (error, results) => {
+    if (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({ message: "Error fetching user" });
+    } else if (results.length === 0) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      const user = results[0]
+      user.imageUrl = await getObjectSignedUrl(user.imageName)
+      res.status(200).json({ user });
+    }
+  });
+});
 
 // Set the server to listen on a specific port
 const PORT = process.env.PORT || 3001;
